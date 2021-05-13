5923c85d3758b9aae3b69e2e6a53d354b72f8a78
jp-taku2 <taku.jp0526@gmail.com>
2021-05-13 21:10:36 +0900

    updat mac's ansible

---
+++ mac/init.sh
@@ -0,0 +1,19 @@
+!/bin/bash
+
+ANSIBLE_PATH=~/work/ansible
+
+## command line tools
+xcode-select --install
+
+## install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+## install ansible
+brew install ansible
+
+## clone repo
+git clone -b template https://github.com/jp-taku2/ansible.git ${ANSIBLE_PATH}
+
+## ansible
+cd ${ANSIBLE_PATH}/mac
+ansible-playbook -i inventory/mac-provisio mac-provisioning.yml
